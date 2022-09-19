de222900697bf7e4fabb5c7b3855cfe5c24f24f1
Andrey Kaizer <andre.kaizer@yandex.ru>
2022-09-20 00:37:00 +0300

    Замена int на long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {500, 25, 150, 490, 725};
+        long[] sales = {500, 25, 150, 490, 725};
 
         SalesManager manager = new SalesManager(sales);
         System.out.println("max: " + manager.max());
